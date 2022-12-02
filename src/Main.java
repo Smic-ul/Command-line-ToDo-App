092946bc52bedda7827dca5ccd5252df5ed08fcd
Stepan.Miculka <stepan.miculka@gmail.com>
2022-12-02 11:17:11 +0100

    Added styleguide

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,27 @@ public class Main {
             }
             case "-a" -> {
                 String task = args[1];
-                if(list.addRecord(task))
+                if(list.addRecord(task)) {
                     System.out.println("Task added successfully");
-                else
+                } else {
                     System.out.println("Task with the given name already exists");
+                }
             }
             case "-r" -> {
                 String task = args[1];
-                if(list.deleteRecord(task))
+                if (list.deleteRecord(task)) {
                     System.out.println("Task deleted successfully");
-                else
+                } else {
                     System.out.println("Task with the given name doesn't exists");
+                }
             }
             case "-c" -> {
                 String task = args[1];
-                if(list.completeTask(task))
+                if (list.completeTask(task)) {
                     System.out.println("Task marked as completed");
-                else
+                } else {
                     System.out.println("Task with the given name doesn't exists");
+                }
             }
 
             default -> System.out.println("No valid option!");
